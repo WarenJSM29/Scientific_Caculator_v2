7b9d70fc394a7a3a69b2a8a72e269c4d243036ab
WarenJSM29 <warrensm87@gmail.com>
2023-09-02 00:05:32 -0400

    New Features Added

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ function DeleteCharacter(event) {
     
     // Verifica si se presionó la tecla Backspace (código de tecla 8)
     if (event.keyCode === 8) {
-        let ActualValue = input.value;
-        input.value = ActualValue.slice(0, -1); // Elimina el último carácter
+        let actualValue = input.value;
+        input.value = actualValue.slice(0, -1); // Elimina el último carácter
     }
 }
 
 // Función para eliminar el último carácter del input de texto al hacer clic en el botón "Eliminar"
-function DeleteLastCharacter() {
+function DeleteCharacterButton() {
     let input = document.getElementById("display");
-    let ActualValue = input.value;
-    input.value = ActualValue.slice(0, -1); // Elimina el último carácter
+    let actualValue = input.value;
+    input.value = actualValue.slice(0, -1); // Elimina el último carácter
 }
 
 //Valor PI
@@ -22,3 +22,43 @@ function PIValue() {
 }
 
 const π = PIValue();
+
+//Funcion Trigonometrica Seno
+function sin(value) {
+    return Math.sin(value);
+}
+
+//Funcion Trigonometrica Coceno
+function cos(value) {
+    return Math.cos(value);
+}
+
+//Funcion Trigonometrica Tangente
+function tan(value) {
+    return Math.tan(value);
+}
+
+//Raiz Cuadrada
+function sqrt(value) {
+    return Math.sqrt(value);
+}
+
+//-------------------------
+function insertText(Text) {
+    let input = document.getElementById("display");
+    let start = input.selectionStart;
+    let end = input.selectionEnd;
+    let value = input.value;
+
+    // Insertar el texto en la posición del cursor
+    let newValue = value.substring(0, start) + Text + value.substring(end);
+
+    // Actualizar el valor del campo de entrada
+    input.value = newValue;
+
+    // Ajustar la posición del cursor después de la inserción
+    input.setSelectionRange(start + Text.length, start + Text.length);
+
+    // Enfocar nuevamente el campo de entrada
+    input.focus();
+}
